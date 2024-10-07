3febfff5ff19e0296c54a78173f0f9d7a566ef0e
Lars <main@rk4z.net>
2024-10-07 16:29:05 +1300

    ちょっと修正（？）

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,12 +1,18 @@
 import adapter from '@sveltejs/adapter-auto';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
 
 	kit: {
-		adapter: adapter()
+		adapter: adapter(),
+		alias: {
+			'@components': path.resolve('src/components'),
+			'@styles': path.resolve('src/styles'),
+			'@routes': path.resolve('src/routes')
+		}
 	}
 };
 
